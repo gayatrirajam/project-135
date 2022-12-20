4177efd084f90a16461d342168e47aec1e6521a5
gayatrirajam <96321688+gayatrirajam@users.noreply.github.com>
2022-12-19 18:26:50 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -6,7 +6,6 @@ function setup(){
     canvas.center();
 
     video = createCapture(VIDEO);
-    video.size(480, 380);
     video.hide();
 }
 function start(){
@@ -20,4 +19,27 @@ function modelLoaded(){
 }
 function draw(){
     image(video, 0, 0, 480, 380);
+    if(status != ""){
+        for(i=0; i<objects.length; i++){
+            fill("red");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("red");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+        if(objectneeded = objects[i].label){
+            videoLiveView.stop();
+            objectDetector.detect(gotResult);
+            document.getElementById("status").innerHTML = "Object Mentioned Found!";
+            var synth = window.speechSynthesis;
+            speak_data = "Object mentioned found";
+            var utterThis = new SpeechSynthesisUtterance(speak_data);
+            synth.speak(utterThis);
+        }
+        else{
+            document.getElementById("status").innerHTML = "Object Mentioned Not Found In Camera";
+        }
+    }
 }
+
